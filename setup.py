cd358485c73bf5a27510482bb2ec5461dafb918a
natus <andrewnech@gmail.com>
2018-05-06 11:21:39 +0300

    added script for updating

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='dnurt_authors_integration',
       version='1.0.0',
       description='DNURT app for authors integration',
       long_description='',
-      install_requires=['psycopg2'],
+      install_requires=['psycopg2', 'elsapy'],
       url='https://github.com/ReturnedVoid/dnurt_authors_integration',
       author='Andrey Nechaev',
       author_email='andrewnech@gmail.com',
       license='DNURT',
-      packages=['dnurtdb', 'scopus', 'web_of_science'],
-      keywords='DNURT author scopus web_of_science'
-)
+      packages=find_packages(),
+      keywords='DNURT author scopus web_of_science',
+
+      entry_points={
+          'console_scripts':
+              ['update = libs.test:update']
+      }
+      )
