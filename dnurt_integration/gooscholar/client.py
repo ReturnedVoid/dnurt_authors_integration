beb0caf2cc6cd787472340ca9ee3a5b70bd3fac4
natus <andrewnech@gmail.com>
2018-05-13 18:51:20 +0300

    initial version of gscholar client

---
+++ dnurt_integration/gooscholar/client.py
@@ -0,0 +1,55 @@
+import bs4 as bs
+from dnurt_integration.dnurtdb import database as db
+import urllib.request
+
+
+class GscholarAuthor:
+    def __init__(self, _id):
+        self.gs_id = _id
+
+    @property
+    def gs_id(self):
+        return self._gs_id
+
+    @gs_id.setter
+    def gs_id(self, val):
+        self._gs_id = val
+
+    @property
+    def doc_count(self):
+        return self._doc_count
+
+    @doc_count.setter
+    def doc_count(self, val):
+        self._doc_count = val
+
+    @property
+    def h_index(self):
+        return self._h_index
+
+    @h_index.setter
+    def h_index(self, val):
+        self._h_index = val
+
+
+def get_author_by_id(_id):
+    source = urllib \
+        .request.urlopen('https://scholar.google.com.ua/citations?user={}&hl=en'
+                         .format('CsPvWKsAAAAJ')).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+    tds = soup.find_all('td')
+
+    author = GscholarAuthor(_id)
+    author.h_index = tds[4].string
+    author.doc_count = '2'
+    return author
+
+
+def update_db():
+    ids = db.get_gs_authors_ids()
+    for _id in ids:
+        if _id:
+            author = get_author_by_id(_id)
+            db.gscholar_update(author)
+
+    db.disconnect()
