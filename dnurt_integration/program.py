8dd8c7be1f043114e08ef93afee49e83e8ca253f
natus <andrewnech@gmail.com>
2018-05-16 21:45:16 +0300

    added reconfiguration ability for database and wos

--- dnurt_integration/program.py
+++ dnurt_integration/program.py
@@ -1,11 +1,15 @@
 # Executive module
 import sys
 from os import system
+import json
 
 from dnurt_integration.scopus import client as sc_client
 from dnurt_integration.web_of_science import client as w_client
 from dnurt_integration.gooscholar import client as g_client
 from dnurt_integration.dnurtdb import database as db
+
+from dnurt_integration.dnurtdb.database import DB_CONFIG_PATH
+from dnurt_integration.web_of_science.client import WOS_CONFIG_PATH
 
 
 def update():
@@ -23,6 +27,10 @@ def update():
         update_gscholar()
     elif arg == '-w':
         update_wos()
+    elif arg == '-cb':
+        reconfigure_db()
+    elif arg == '-cw':
+        reconfigure_wos()
     else:
         update_all()
 
@@ -52,6 +60,28 @@ def update_wos():
     print('Updating wos info...')
     w_client.update_db()
     print('Done.')
+
+
+def reconfigure_db():
+    with open(DB_CONFIG_PATH, "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    data['is_initialized'] = False
+
+    with open(DB_CONFIG_PATH, "w") as jsonFile:
+        json.dump(data, jsonFile)
+    db.init_db()
+
+
+def reconfigure_wos():
+    with open(WOS_CONFIG_PATH, "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    data['is_initialized'] = False
+
+    with open(WOS_CONFIG_PATH, "w") as jsonFile:
+        json.dump(data, jsonFile)
+    w_client.init_wos_config()
 
 
 if __name__ == '__main__':
